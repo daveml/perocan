a23c0b08ce6cc21b98e5b0afe9c355ce9f5bbe3f
Dave Lawrence <davemlnj@gmail.com>
2019-07-05 11:06:09 -0400

    Adding roborio header

---
+++ perocan_roborio.h
@@ -0,0 +1,31 @@
+/*
+  perocan
+  HAL_perocan.h
+*/
+#ifndef PEROCAN_ARDUINO_H
+#define PEROCAN_ARDUINO_H
+
+#include <Arduino.h>
+#include "perocan.h"
+#include <FlexCAN.h>
+
+namespace perocan
+{
+	
+class perocan_arduino : public perocan__base
+{
+public:
+  bool init(uint8_t DevType, uint8_t DevMfr, uint8_t DevId);
+  bool init() {
+	return init(perocan::defaultDevType, perocan::defaultDevMfr, perocan::defaultDevId); }
+  bool send(uint8_t *Data, int Len, int Api);
+  bool recv(perocan_message_t *Msg, uint16_t Api);
+  bool available();
+  
+private:
+  void copy_rxMsg(perocan_message_t *Msg, CAN_message_t RxMsg);
+  void copy_txMsg(CAN_message_t *TxMsg, perocan_message_t *Msg);
+};
+
+}
+#endif
