c4c6e39580c2f0c773a67daabc59b2e9835c8286
Dave Lawrence <davemlnj@gmail.com>
2019-07-05 11:01:58 -0400

    Ading agnostic types

--- perocan.h
+++ perocan.h
@@ -5,14 +5,7 @@
 #ifndef HAL_PEROCAN_H
 #define HAL_PEROCAN_H
 
-//#include <Arduino.h>
-#include "test.h"
-
-#define HAL_CAN_USES_TEENSY
-//#define HAL_CAN_USES_ROBORIO
-//#define HAL_CAN_USES_GENERICARDUINO
-
-#include <FlexCAN.h>
+#include "perocan_types.h"
 
 namespace perocan
 {
@@ -30,21 +23,17 @@ typedef struct {
   uint16_t timeout; // milliseconds, zero will disable waiting
   uint8_t buf[8];
   bool IsNew;
-} HAL_CAN__Message_t;
+} perocan_message_t;
 
 const uint8_t defaultDevType = 0x0F;
 const uint8_t defaultDevMfr  = 0x03;
 const uint8_t defaultDevId   = 0x01;
 
-class HAL_CAN
+class perocan__base
 {
 public:
 
-  bool init(uint8_t DevType, uint8_t DevMfr, uint8_t DevId);
-  bool init() ;
-  bool send(uint8_t *Data, int Len, int Api);
-  bool recv(HAL_CAN__Message_t *Msg, uint16_t Api);
-  bool available();
+  bool init(uint8_t DevType, uint8_t DevMfr, uint8_t DevId){ return false;}
   
   void register_API(uint16_t Api) {
     Buffer[ApiCount].IsNew = false;
@@ -52,7 +41,6 @@ public:
     ApiCount++;
   }
   
- private:
   uint32_t CreateCANId(CANStorage_t* storage, unsigned apiId) {
       return packCANId(storage->deviceType, storage->manufacturer, apiId, storage->deviceId);
   }
@@ -63,8 +51,6 @@ public:
   uint32_t Id_getFrom_Api(uint16_t Api) {
 	  return CANApiMask | (Api << 6);
   }
-  void copy_rxMsg(HAL_CAN__Message_t *Msg, CAN_message_t RxMsg);
-  void copy_txMsg(CAN_message_t *TxMsg, HAL_CAN__Message_t *Msg);
 
   int Api_find_Registered(uint16_t Api) {
     for(int idx=0; idx<ApiCount; idx++)
@@ -85,7 +71,7 @@ public:
   }
 
   uint16_t ApiCount;
-  HAL_CAN__Message_t Buffer[8];
+  perocan_message_t Buffer[8];
   uint16_t BufferedMsgCount;
   uint32_t CANApiMask;
 };
