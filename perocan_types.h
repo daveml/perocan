411ef62769e02ede1975e06a5d4e0440c947b7f9
Dave Lawrence <davemlnj@gmail.com>
2019-07-05 10:54:44 -0400

    Adding Rio support

---
+++ perocan_types.h
@@ -0,0 +1,34 @@
+#ifndef PEROCAN_TYPES_H
+#define PEROCAN_TYPES_H
+
+#ifdef uint8_t
+#undef uint8_t
+#endif
+#define uint8_t unsigned char
+
+#ifdef int8_t
+#undef int8_t
+#endif
+#define int8_t char
+
+#ifdef uint16_t
+#undef uint16_t
+#endif
+#define uint16_t unsigned short
+
+#ifdef int16_t
+#undef int16_t
+#endif
+#define int16_t short
+
+#ifdef uint32_t
+#undef uint32_t
+#endif
+#define uint32_t unsigned long
+
+#ifdef int32_t
+#undef int32_t
+#endif
+#define int32_t long
+
+#endif
